8033f541f7fec8dc28424d79f8dc9adb2ad8d3b7
jakir-ruet <jakir.ruet.bd@gmail.com>
2024-02-20 16:09:53 +0600

    crud completed

---
+++ README.md
@@ -0,0 +1,19 @@
+### PHP Laravel CRUD App for DevOps Testing
+
+## Run the Application
+```bash
+npm install
+```
+```bash
+composer update
+```
+```bash
+php artisan migrate
+```
+```bash
+php artisan serve
+```
+If we need
+```bash
+php artisan db:seed
+```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Welcome to Student CRUD</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body class="container text-center mt-4 pt-4">
+        <a href="{{ url('/student') }}" class="btn btn-success" title="Add New Student">Welcome to Student CRUD</a>
+    </body>
+</html>
